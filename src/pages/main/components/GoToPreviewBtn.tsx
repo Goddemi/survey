2a76822fcc5d200a13e8020198705fb26eaf9577
goddemi <goddemi0124@gmail.com>
2023-03-12 12:06:27 +0900

    Modify : 미리보기, 제출 버튼 위치수정

--- src/pages/main/components/GoToPreviewBtn.tsx
+++ src/pages/main/components/GoToPreviewBtn.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const GoToPreviewBtn = () => {
   return (
     <Link to={"/preview"}>
-      <button className="mt-3 p-4 bg-orange-500 text-white font-semibold rounded-lg ">
+      <button className="mt-3 mb-7 p-4 bg-orange-500 text-white font-semibold rounded-lg ">
         미리보기
       </button>
     </Link>
